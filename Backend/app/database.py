1464951a647205ea8f2d1d78f828b684c27659d0
RR-commit <RaveeRaghul.C@ust.trumio.ai>
2026-01-17 23:50:04 +0530

    Working Code

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -5,7 +5,8 @@ DATABASE_URL = "sqlite:///./library.db"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {},
+    connect_args={"check_same_thread": False} 
+    if DATABASE_URL.startswith("sqlite") else {},
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
